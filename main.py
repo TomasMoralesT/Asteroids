9380fc1517b70003bdff9b832e54bebab34f68bb
GNight <tomas11_92@hotmail.com>
2024-09-26 13:39:57 -0500

    G: Ch 2, 8 Groups

--- main.py
+++ main.py
@@ -6,17 +6,29 @@ def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
-	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-	dt = 0
 	
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	
+	Player.containers = (updatable, drawable)
+
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+		
+	dt = 0
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 
+		for item in updatable:
+			item.update(dt)
+
 		screen.fill((0,0,0))
-		player.draw(screen)
-		player.update(dt)
+
+		for item in drawable:
+			item.draw(screen)
+
 		pygame.display.flip()
 
 		dt = clock.tick(60) / 1000
