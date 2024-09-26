7cb728efcfa7b7cd97725a47641d8b2b7f0fe005
GNight <tomas11_92@hotmail.com>
2024-09-26 15:11:45 -0500

    K: Ch 3, 5 Destruction

--- main.py
+++ main.py
@@ -36,6 +36,11 @@ def main():
 			item.update(dt)
 		
 		for asteroid in asteroids:
+			for shot in shots:
+				if asteroid.collision(shot):
+					asteroid.kill()
+					shot.kill()
+					
 			if asteroid.collision(player):
 				print("Game over!")
 				sys.exit()
